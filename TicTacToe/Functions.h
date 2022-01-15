59bd88cfce7620bfedb3b1aae2a15e848fb1224e
Mohammad Almanjomi <62033792+isurg@users.noreply.github.com>
2022-01-15 16:02:03 +0300

    Added so much, forgot to commit and push early on. I have never used git before.

--- TicTacToe/Functions.h
+++ TicTacToe/Functions.h
@@ -2,6 +2,128 @@
 
 void greet() {
 
+	std::cout << "_______________________\n\n";
+	std::cout << "Welcome to Tic-Tac-Toe!\n\n";
+	std::cout << "ASCII ART\n\n";
+	std::cout << "_______________________\n\n\n";
+
+}
 
 
+void make_spots(int grid_array[3][3]) {
+
+	int num = 1;
+
+	for (int i = 0; i < 3; i++) {
+		
+		for (int j = 0; j < 3; j++) {
+
+			grid_array[i][j] = num;
+			num++;
+		}
+	}
+}
+
+
+void draw_board(int grid_array[3][3]) {
+
+	for (int i = 0; i < 3; i++) {
+
+		for (int j = 0; j < 3; j++) {
+
+			if (grid_array[i][j] == 0)
+
+			std::cout << " " << grid_array[i][j];
+
+			if (j != 2)
+				std::cout << "  | ";
+
+		}
+
+		if (i != 2)
+			std::cout << "\n----------------\n";
+	}
+
+	std::cout << "\n\n";
+}
+
+int turn_function(int turn_num) {
+
+	if (turn_num % 2)
+		return 0;
+	else
+		return 10;
+	
+}
+
+
+
+void choose(int grid_array[3][3], int turn) {
+
+	int choice;
+	char turn_char;
+	if (turn == 0)
+		turn_char = 'X';
+	else
+		turn_char = 'O';
+
+
+	std::cout << "\n It's " << turn_char << "'s turn, choose a spot: ";
+	std::cin >> choice;
+	std::cout << "\n\n";
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+
+			if ( grid_array[i][j] == (choice) ) {
+				
+				grid_array[i][j] = turn;
+			}
+		}
+	}
+
+}
+
+
+
+bool win_checker(int grid_array[3][3]) {
+
+	// 8 sequences
+	for (int i = 0; i < 3; i++) {
+
+		// 3 sequences, rows
+		if (grid_array[i][0] == grid_array[i][1] == grid_array[i][2])
+			return true;
+		
+		// 3 sequences
+		if (grid_array[0][i] == grid_array[1][i] == grid_array[2][i])
+			return true;
+		
+		// Check it once, as it not related to the loop
+		if (i == 0) {
+
+			// 1 sequence, decline diagonal
+			if (grid_array[0][0] == grid_array[1][1] == grid_array[2][2])
+				return true;
+			
+			// 1 sequence, incline diagonal
+			if (grid_array[2][0] == grid_array[1][1] == grid_array[0][2])
+				return true;
+		}
+
+	}
+	return false;
+}
+
+
+
+void end_game(bool win, int turn) {
+
+	// if any had won
+	if (win == true) {
+		std::cout << "The winner is " << turn << ", Congrats!\n";
+	}
+	else {
+		std::cout << "It's a tie!\n";
+	}
 }
--- TicTacToe/Main.cpp
+++ TicTacToe/Main.cpp
@@ -4,12 +4,44 @@
 
 int main()
 {
+	// just greeting
+	greet();
 
-	// greet()
-	// draw_board
-	// choose()
-	// 
-
+	// make a 3x3 grid, to choose from
+	int grid_array[3][3]; 
 	
+	// to fill in numbers of each spot
+	make_spots(grid_array);
+
+
+	// simply, 9 spots, 9 turns, even number is 'X', odd is 'O'
+	int turn_num = 0;
+	
+	// represents the turn in character X or O
+	int turn = 0;
+
+	// starts as false, at the the end the function win_checker checks if any won, then return true
+	bool win = false;
+
+
+	while (win == false && turn_num < 9) {
+
+		turn = turn_function(turn_num);
+
+		// self explanatory, draws board with the available spots
+		draw_board(grid_array);
+
+		choose(grid_array, turn);
+
+		// check
+		win = win_checker(grid_array);
+		
+		if (win == false)
+			turn_num++;
+		
+	}
+
+	draw_board(grid_array);
+	end_game(win, turn);
 
 }
